8298573b8d430a88be873df3139ddcded727dd90
Jijo James <jijojames18@gmail.com>
2021-04-13 12:04:41 +0530

    Boilerplate for popup

---
+++ popup.js
@@ -0,0 +1,9 @@
+const extensionWindow = (function () {
+  if (typeof chrome !== undefined) {
+    return chrome;
+  } else if (typeof browser !== undefined) {
+    return browser;
+  } else if (typeof msBrowser !== undefined) {
+    return msBrowser;
+  }
+})();
