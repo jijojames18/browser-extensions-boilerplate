944fef5e8fcb4d5b61c5d5d92e892288b7b7dea8
Jijo James <jijojames18@gmail.com>
2021-04-18 13:22:00 +0530

    Empty directory on build

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,8 @@
 var webpack = require("webpack"),
-  config = require("../webpack.config");
+  config = require("../webpack.config"),
+  fs = require("fs-extra");
+
+fs.emptyDirSync("dist");
 
 webpack(config, function (err) {
   if (err) throw err;
